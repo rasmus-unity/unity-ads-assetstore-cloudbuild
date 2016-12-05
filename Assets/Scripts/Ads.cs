702a2e3257840db5cc4f6636eb622d0c743b6613
Rasmus Selsmark <rasmus@unity3d.com>
2016-12-05 13:25:33 +0200

    Calling our fake Advertisement class

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ads : MonoBehaviour
+{
+    void Start ()
+    {
+        Debug.Log ("Calling our fake Advertisement.Initialize() method");
+        UnityEngine.Advertisements.Advertisement.Initialize ("14851");
+    }
+}
